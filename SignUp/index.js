d7916c75c0821894c43be74db68cb57efa333fdf
Subham Das <subhamdas461@gmail.com>
2021-01-03 11:46:32 +0530

    added validation and more

--- SignUp/index.js
+++ SignUp/index.js
@@ -2,21 +2,51 @@ const signupForm = document.querySelector('#signupForm');
 const popup = document.querySelector('.popup');
 const verifyPhn = document.querySelector('.verify-phn');
 const verifyOtp = document.querySelector('.verify-otp');
+const otp_input = document.querySelectorAll('#otp input')
+const pass = document.querySelectorAll("input[type=password]")
+const signupInput = document.querySelectorAll('.inp')
+const errorLine = document.querySelectorAll('#signupForm div p')
 
+console.log(errorLine)
 document.querySelector('#otp').addEventListener('submit',e=>{
     e.preventDefault()
 })
 signupForm.addEventListener('submit',e=>{
     e.preventDefault()
 })
-
-
 verifyPhn.addEventListener('click', e =>{
-    
-    
-    popup.classList.add('popup-visible')
+    let error;
+    signupInput.forEach(e=>{
+        if(e.value == ""){
+           
+            e.parentElement.children[1].style.display = "block" ;
+            error = true;
+        }
+        else{
+            e.parentElement.children[1].style.display = "none" ;
+        }
+        if(pass[0].value != pass[1].value ){
+            error=true
+            e.parentElement.children[1].style.display = "none"
+            errorLine[7].style.display = "block"
+        }
+        else{
+            errorLine[7].style.display = "none"  
+        }
+        
+    })
+    if(error){
+        return
+    }
+    // if(pass[0].value != pass[1].value ){
+    //     return errorLine[7].style.display = "block"
+    // }else{
+    //      errorLine[7].style.display = "none"  
+    // }
 
-    document.querySelectorAll('#signupForm input').forEach(e=>{
+    // show popup
+    popup.classList.add('popup-visible')
+    document.querySelectorAll('#signupForm .inp').forEach(e=>{
         e.value = ""
     })
     document.querySelectorAll('#otp input').forEach(e=>{
@@ -29,7 +59,25 @@ function otpEnterEvent(item,id){
     if(item.value.length){
         document.getElementById(id).focus();
     }
-    else{
-        console.log("empty")
-    }
 }
+function clearInput(item,id){
+   item.addEventListener('keyup',e=>{
+       if(e.key === "Backspace"){
+           if(id > 0){
+               if(id == 3){
+                   item.value = ""
+               }
+               otp_input[id-1].focus()
+           }
+       }
+    //    if(e.key === "ArrowLeft"){
+    //     if(id > 0)
+    //        otp_input[id-1].focus()
+    //     }
+    //     if(e.key === "ArrowRight"){
+    //         if(id < 3)
+    //        otp_input[id+1].focus()
+    //    }
+   })
+}
+
